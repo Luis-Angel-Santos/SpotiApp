e1fbbcf83b9d4797685e61c5d54f6e3e08fabce0
Luis Angel <santosangel748@gmail.com>
2020-10-06 23:08:42 -0500

    Widgets de Spotify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SpotifyService } from './services/spotify.service';
 import { NoimagePipe } from './pipes/noimage.pipe';
 import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import { LoadingComponent } from './components/shared/loading/loading.component'
     NavbarComponent,
     NoimagePipe,
     TarjetasComponent,
+    DomseguroPipe,
     LoadingComponent
   ],
   imports: [
